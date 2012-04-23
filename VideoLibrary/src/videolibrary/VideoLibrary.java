e27437c10521b93252600754a1e4592ad771730d
ScHuMinsen@gmail.com <ScHuMinsen@gmail.com@f6fdefa8-1b2b-b96a-5035-86b76eb7f3cc>
2012-04-23 10:56:00 +0000

    test

---
+++ VideoLibrary/src/videolibrary/VideoLibrary.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videolibrary;
+
+/**
+ *
+ * @author Andrej
+ */
+public class VideoLibrary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
