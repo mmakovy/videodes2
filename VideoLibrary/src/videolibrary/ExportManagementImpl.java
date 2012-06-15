2192d1ebc69828660ad45d2b7e5c5aaa10cf6e99
ScHuMinsen@gmail.com <ScHuMinsen@gmail.com@f6fdefa8-1b2b-b96a-5035-86b76eb7f3cc>
2012-06-15 17:28:01 +0000

    upraveny export tak, aby exportoval aj prazdny dokument

--- VideoLibrary/src/videolibrary/ExportManagementImpl.java
+++ VideoLibrary/src/videolibrary/ExportManagementImpl.java
@@ -13,92 +13,100 @@ import org.odftoolkit.simple.SpreadsheetDocument;
  *
  * @author Samo
  */
-public class ExportManagementImpl implements ExportManagement{
+public class ExportManagementImpl implements ExportManagement {
+
     private List<Video> videoList = null;
-    
+
     public void setList(List videos) {
         this.videoList = videos;
     }
+
     @Override
     public boolean exportToOdf(File file) {
-        try{
+        try {
             SpreadsheetDocument odf = SpreadsheetDocument.newSpreadsheetDocument();
-            
-        String[][] data = new String[videoList.size()+1][7];
-        data[0][0] = "Title";
-        data[0][1] = "Director(s)";
-        data[0][2] = "Actor(s)";
-        data[0][3] = "Year";
-        data[0][4] = "Countries";
-        data[0][5] = "Genres";
-        data[0][6] = "Rating";
-        int i = 0;
-        for (Video video : videoList){
-                        
-            data[i+1][0] = video.getTitle();
-            
-            List<String> directorsList = video.getDirectors();
-            String directors = "";
-            int k=0;
-            for (String director : directorsList){
-                if (k==0) {directors = director;
-                } else {
-                directors = directors + ", " + director;
-                }
-                k++;
-            }
-            data[i+1][1] = directors;
-            k=0;
-            List<String> actorsList = video.getActors();
-            String actors = "";
-            for (String actor : actorsList){
-                if (k==0){actors=actor;
-                    } else {
-                    actors = actors + "," + actor;
-                    }
-            k++;
-            }
-            data[i+1][2] = actors;
-            
-            data[i+1][3] = Integer.toString(video.getYear());
-            
-            k=0;
-            List<String> countriesList = video.getCountries();
-            String countries = "";
-            for (String country : countriesList){
-                if (k==0){
-                countries=country;
-                } else {
-                    countries = countries + ", " + country;
-                }
-                k++;
-            }
-            data[i+1][4] = countries;
-            
-            k=0;
-            List<Genre> genresList = video.getGenres();
-            String genres = "";
-            for (Genre genre : genresList){
-                if (k==0){genres=genre.toString();
-                } else {
-                    genres = genres + ", " + genre.toString();
-                }
-                k++;
-            }
-            data[i+1][5] = genres;
-            
-            data[i+1][6] = Integer.toString(video.getRating());
-            i++;
-        }
-        
-        odf.getTableByName("Sheet1").remove();
-        Table videoTable = Table.newTable(odf, null, null, data);
-        videoTable.setTableName("Videos");
-        videoTable.getCellByPosition(0, 0).setStringValue("Title");
-        
-        odf.save(file);
-        return true;
-        }  catch (Exception e) { e.printStackTrace(); return false;}            
-    }
+            if (videoList != null) {
+                String[][] data = new String[videoList.size() + 1][7];
+                data[0][0] = "Title";
+                data[0][1] = "Director(s)";
+                data[0][2] = "Actor(s)";
+                data[0][3] = "Year";
+                data[0][4] = "Countries";
+                data[0][5] = "Genres";
+                data[0][6] = "Rating";
+                int i = 0;
 
+                for (Video video : videoList) {
+
+                    data[i + 1][0] = video.getTitle();
+
+                    List<String> directorsList = video.getDirectors();
+                    String directors = "";
+                    int k = 0;
+                    for (String director : directorsList) {
+                        if (k == 0) {
+                            directors = director;
+                        } else {
+                            directors = directors + ", " + director;
+                        }
+                        k++;
+                    }
+                    data[i + 1][1] = directors;
+                    k = 0;
+                    List<String> actorsList = video.getActors();
+                    String actors = "";
+                    for (String actor : actorsList) {
+                        if (k == 0) {
+                            actors = actor;
+                        } else {
+                            actors = actors + "," + actor;
+                        }
+                        k++;
+                    }
+                    data[i + 1][2] = actors;
+
+                    data[i + 1][3] = Integer.toString(video.getYear());
+
+                    k = 0;
+                    List<String> countriesList = video.getCountries();
+                    String countries = "";
+                    for (String country : countriesList) {
+                        if (k == 0) {
+                            countries = country;
+                        } else {
+                            countries = countries + ", " + country;
+                        }
+                        k++;
+                    }
+                    data[i + 1][4] = countries;
+
+                    k = 0;
+                    List<Genre> genresList = video.getGenres();
+                    String genres = "";
+                    for (Genre genre : genresList) {
+                        if (k == 0) {
+                            genres = genre.toString();
+                        } else {
+                            genres = genres + ", " + genre.toString();
+                        }
+                        k++;
+                    }
+                    data[i + 1][5] = genres;
+
+                    data[i + 1][6] = Integer.toString(video.getRating());
+                    i++;
+                }
+
+                odf.getTableByName("Sheet1").remove();
+                Table videoTable = Table.newTable(odf, null, null, data);
+                videoTable.setTableName("Videos");
+                videoTable.getCellByPosition(0, 0).setStringValue("Title");
+            }
+            odf.save(file);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
